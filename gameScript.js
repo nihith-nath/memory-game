f1f9d984e0859bfe3a2231a46b1e2c7a7a05815b
nihith-nath <109498738+nihith-nath@users.noreply.github.com>
2022-11-11 01:42:05 +0530

    Add files via upload

--- gameScript.js
+++ gameScript.js
@@ -24,10 +24,17 @@ function init() {
 window.onload = function() {
     $("#ol").html(`<center><div id="inst"><h3>Welcome !</h3>Instructions For Game<br/><br/><li>Make pairs of similiar blocks by flipping them.</li><li>To flip a block you can click on it.</li><li>If two blocks you clicked are not similar, they will be flipped back.</li><p style="font-size:18px;">Click one of the following mode to start the game.</p></div><button onclick="start(3, 4)">3 x 4</button> <button onclick="start(4, 4)" style="w">4 x 4</button><button onclick="start(4, 5)">4 x 5</button><button onclick="start(5, 6)">5 x 6</button><button onclick="start(6, 6)">6 x 6</button></center>`);
 }
-
+ function restart()
+{
+q =false;
+  $("#ol").fadeIn(750);
+}
+var q=false;
 //Starting the game
 function start(r,l) {
     //Timer and moves
+    quit = true;
+
     min=0, sec=0, moves=0;
     $("#time").html("Time: 00:00");
     $("#moves").html("Moves: 0");
@@ -36,9 +43,9 @@ function start(r,l) {
       if(sec==60) {
           min++; sec=0;
       }
-      if(sec<10) 
+      if(sec<10)
           $("#time").html("Time: 0"+min+":0"+sec);
-      else 
+      else
         $("#time").html("Time: 0"+min+":"+sec);
     }, 1000);
     rem=r*l/2, noItems=rem;
@@ -56,7 +63,7 @@ function start(r,l) {
         items[c] = items[p];
         items[p] = tmp;
     }
-    
+
     //Creating table
     $("table").html("");
     var n=1;
@@ -68,28 +75,31 @@ function start(r,l) {
          }
          $("table").append("</tr>");
     }
-    
+
     //Hiding instructions screen
     $("#ol").fadeOut(500);
 }
+while(q)
+{
 
+}
 //Function for flipping blocks
 function change(x) {
   //Variables
   let i = "#"+x+" .inner";
   let f = "#"+x+" .inner .front";
   let b = "#"+x+" .inner .back";
-  
+
   //Dont flip for these conditions
   if (turn==2 || $(i).attr("flip")=="block" || ppID==x) {}
-  
+
   //Flip
   else {
     $(i).css(t, flip);
     if (turn==1) {
       //This value will prevent spam clicking
       turn=2;
-      
+
       //If both flipped blocks are not same
       if (pre!=$(b).text()) {
          setTimeout(function() {
@@ -98,21 +108,21 @@ function change(x) {
             ppID=0;
          },1000);
       }
-      
+
       //If blocks flipped are same
       else {
           rem--;
           $(i).attr("flip", "block");
           $(pID).attr("flip", "block");
       }
-      
+
       setTimeout(function() {
          turn=0;
          //Increase moves
          moves++;
          $("#moves").html("Moves: "+moves);
       },1150);
-      
+
     }
     else {
       pre = $(b).text();
@@ -120,7 +130,7 @@ function change(x) {
       pID = "#"+x+" .inner";
       turn=1;
     }
-    
+
     //If all pairs are matched
     if (rem==0) {
           clearInterval(time);
